87f40eb5fda9116dbebddf0a2bb610a362a3074a
Mark Boudreau <mboudreaudalton@gmail.com>
2023-01-23 13:17:33 -0500

    catch state change events

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/mbd98/nm-captive
+
+require (
+	github.com/Wifx/gonetworkmanager/v2 v2.1.0
+	github.com/godbus/dbus/v5 v5.1.0
+)
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/Wifx/gonetworkmanager/v2"
+	"log"
+)
+
+func main() {
+	nm, err := gonetworkmanager.NewNetworkManager()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	ch := nm.Subscribe()
+	for {
+		sig := <-ch
+		if sig.Name == "org.freedesktop.NetworkManager.Device.StateChanged" {
+			state := gonetworkmanager.NmState(sig.Body[0].(uint32))
+			if state == gonetworkmanager.NmStateConnectedSite {
+				err := nm.CheckConnectivity()
+				if err != nil {
+					log.Fatalln(err)
+				}
+				connectivity, err := nm.GetPropertyConnectivity()
+				if err != nil {
+					log.Fatalln(err)
+				}
+				if connectivity == gonetworkmanager.NmConnectivityPortal {
+					log.Println("Captive portal detected")
+				} else {
+					log.Println("Other connectivity state")
+				}
+			}
+		} else {
+			log.Println("Other state change")
+		}
+	}
+}
